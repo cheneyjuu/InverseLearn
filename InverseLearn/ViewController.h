a2dd9289b5407b943804d182a6ee3143817b7a90
大知 <dazhi@dazhideiMac.local>
2014-07-22 10:35:28 +0800

    Initial Commit

---
+++ InverseLearn/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InverseLearn
+//
+//  Created by 大知 on 14-7-22.
+//  Copyright (c) 2014年 DaZhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ InverseLearn/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InverseLearn
+//
+//  Created by 大知 on 14-7-22.
+//  Copyright (c) 2014年 DaZhi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
